1b8091c6db333f26a5769cb0766f3c10c24f3b06
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-07-30 17:17:37 -0700

    Fixed division to return int

--- 4_Searching_and_sorting/Binary_search.py
+++ 4_Searching_and_sorting/Binary_search.py
@@ -15,7 +15,7 @@ def binary_search(input_array, value):
     start=0
     end=len(input_array)-1
     while(start<=end):
-        mid=(start+end)/2
+        mid=(start+end)//2 # returns int value
         if input_array[mid]==value:
             return mid
         elif input_array[mid]>value:
@@ -23,3 +23,9 @@ def binary_search(input_array, value):
         elif input_array[mid]<value:
             start=mid+1
     return -1
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print (binary_search(test_list, test_val1))
+print (binary_search(test_list, test_val2))
