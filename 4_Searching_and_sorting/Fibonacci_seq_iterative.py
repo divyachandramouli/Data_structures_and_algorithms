fd429f434c44ea184d6b57a07288d32c9206182a
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-01 14:20:15 -0700

    Created fibonacci seq generator - iterative

---
+++ 4_Searching_and_sorting/Fibonacci_seq_iterative.py
@@ -0,0 +1,20 @@
+#Generate fibonacci seq with index as input
+
+def getfib(pos):
+	
+	fib=[0,1]
+	first=fib[0]
+	second=fib[1]
+	if (pos==0):
+		return fib[0]
+	if (pos==1):
+		return fib
+	for i in range (2,pos+1):
+		next=first+second
+		first=second
+		second=next
+		fib.append(next)
+	return fib
+
+
+print(getfib(2))
