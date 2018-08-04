5232279d3caa69518a539b0a223144b167c2ba3c
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-04 15:47:37 -0700

    Set up bubble sort test case

--- 4_Searching_and_sorting/Bubble_sort/bubble_sort_v1.py
+++ 4_Searching_and_sorting/Bubble_sort/bubble_sort_v1.py
@@ -1,10 +1,11 @@
-def bubble_sorter(arr)
+def bubble_sorter(arr):
 	n=len(arr)
-	for i in range(0,n):
-		if (arr[i]>arr[i+1]):
-			temp=arr[i]
-			arr[i]=arr[i+1]
-			arr[i+1]=temp
+	for j in range(0,n-1):
+		for i in range(0,n-1):
+			if (arr[i]>arr[i+1]):
+				temp=arr[i]
+				arr[i]=arr[i+1]
+				arr[i+1]=temp
 	return arr
 
 array1=[21,4,1,3,9,20,25,6,21,14]
