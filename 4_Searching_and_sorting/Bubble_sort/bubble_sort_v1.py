c9510e025e87879f3c684d924b40c0b2e8c91ba0
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-05 15:58:08 -0700

    Remove temp var for swap, reduce iterations

--- 4_Searching_and_sorting/Bubble_sort/bubble_sort_v1.py
+++ 4_Searching_and_sorting/Bubble_sort/bubble_sort_v1.py
@@ -1,12 +1,17 @@
+# Implementation of bubble sort 
+
 def bubble_sorter(arr):
 	n=len(arr)
+	i=0
 	for j in range(0,n-1):
-		for i in range(0,n-1):
+		for i in range(0,n-i-1):
+			#In the  ith iteration, last i elements have bubbled up
 			if (arr[i]>arr[i+1]):
-				temp=arr[i]
-				arr[i]=arr[i+1]
-				arr[i+1]=temp
+				
+				arr[i],arr[i+1]=arr[i+1],arr[i]
+				
 	return arr
+
 
 array1=[21,4,1,3,9,20,25,6,21,14]
 print(bubble_sorter(array1))
