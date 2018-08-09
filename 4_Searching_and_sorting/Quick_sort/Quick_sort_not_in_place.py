88eb44277146f14efcee3992dbcf584fee4e5e81
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-09 15:28:29 -0700

    Add comments

--- 4_Searching_and_sorting/Quick_sort/Quick_sort_not_in_place.py
+++ 4_Searching_and_sorting/Quick_sort/Quick_sort_not_in_place.py
@@ -3,16 +3,18 @@ from random import randint
 def quicksort(a):
 	if len(a)<=1:
 		return a
-		smaller,equal,larger=[],[],[]
-		pivot=a[randint(0,len(a)-1)]
-		for x in a:
-			if x<pivot:
-				smaller.append(x)
-			elif x>pivot:
-				larger.append(x)
-			else:
-				equal.append(x)
-		return quicksort(smaller)+equal+quicksort(larger)
+	#create auxillary arrays
+	smaller,equal,larger=[],[],[]
+	#select random pivot
+	pivot=a[randint(0,len(a)-1)]
+	for x in a:
+		if x<pivot:
+			smaller.append(x)
+		elif x>pivot:
+			larger.append(x)
+		else:
+			equal.append(x)
+	return quicksort(smaller)+equal+quicksort(larger)
 
 arr1=[8,3,1,7,0,10,2]
 print(quicksort(arr1))
