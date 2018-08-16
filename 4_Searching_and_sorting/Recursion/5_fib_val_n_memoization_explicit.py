ef2b14d81b020fc97cddd805b4cdfa6538bffb9b
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-15 17:39:47 -0700

    Check cached values before computing

--- 4_Searching_and_sorting/Recursion/5_fib_val_n_memoization_explicit.py
+++ 4_Searching_and_sorting/Recursion/5_fib_val_n_memoization_explicit.py
@@ -1,16 +1,23 @@
 #Implement memoization explicitly 
 
 
-fib_cached=[]
+fib_cached={}
 def fib(n):
-	if n==1:
-		value=1
-	if n==2:
-		value=2
-	if n>2:
-		value = fib(n-1)+fib(n-2)
+	#If we already have the cached value return it
+	if n in fib_cached:
+		return fib_cached[n]
+	else:
+		#Compute the nth term, cache it, then return it
+		if n==1:
+			value=1
+		if n==2:
+			value=2
+		if n>2:
+			value = fib(n-1)+fib(n-2)
 
-	fib_cached[n]=value
+		fib_cached[n]=value
 
-	return value
+		return value
 
+for i in range(1,1001):
+	print(i,':',fib(i))
