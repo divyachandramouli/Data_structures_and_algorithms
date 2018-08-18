4a5558c40f38e2eed432bfb7dbf6c1c0ad211042
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-08-17 17:52:19 -0700

    Check input validity

--- 4_Searching_and_sorting/Recursion/fib_val_builtin_memoization.py
+++ 4_Searching_and_sorting/Recursion/fib_val_builtin_memoization.py
@@ -1,10 +1,17 @@
 #Implement memoization explicitly 
 
-from functools import lru_cache
+from functools import lru_cache #Least Recently Used Cache - a decorator
+#By default it caches 128 most recent function calls - specify #values to cache 
 
-@lru_cache()
+@lru_cache(maxsize=1000)
 
 def fib(n):
+#Check that the input is a positive integer
+	if type(n) != int
+		raise TypeError "n must be a positive integer"
+# No fraction values are accepted 
+	if n<1:
+		raise TypeError "n must be a positive integer"
 	if n==1:
 			return 1
 	if n==2:
